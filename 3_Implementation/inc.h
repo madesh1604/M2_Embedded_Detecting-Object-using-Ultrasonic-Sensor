4363ac8e08e94586d311dbc78babdbfedfcf10be
Sankar Narayanan <91029826+madesh1604@users.noreply.github.com>
2021-12-01 18:04:07 +0530

    Add files via upload

---
+++ 3_Implementation/inc.h
@@ -0,0 +1,14 @@
+#ifndef __OBJECT_H_
+#define __OBJECT_H_
+#define F_CPU 1000000
+#include <util/delay.h>
+#include <stdlib.h>
+
+#define enable            5
+#define registerselection 6
+
+void send_a_command(unsigned char command);
+void send_a_character(unsigned char character);
+void send_a_string(char *string_of_characters);
+
+#endif
---
+++ 3_Implementation/src.c
@@ -0,0 +1,26 @@
+void send_a_command(unsigned char command)
+{
+    PORTB = command;
+    PORTD &= ~ (1<<registerselection);
+    PORTD |= 1<<enable;
+    _delay_ms(8);
+    PORTD &= ~1<<enable;
+    PORTB = 0;
+}
+
+void send_a_character(unsigned char character)
+{
+    PORTB = character;
+    PORTD |= 1<<registerselection;
+    PORTD |= 1<<enable;
+    _delay_ms(8);
+    PORTD &= ~1<<enable;
+    PORTB = 0;
+}
+void send_a_string(char *string_of_characters)
+{
+    while(*string_of_characters > 0)
+    {
+        send_a_character(*string_of_characters++);
+    }
+}
