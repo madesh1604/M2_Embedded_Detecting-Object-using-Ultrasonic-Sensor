12ccd2350ef4fc3086d4b2b506990102e35eb8e6
Sankar Narayanan <91029826+madesh1604@users.noreply.github.com>
2021-12-01 22:50:58 +0530

    Add files via upload

---
+++ 3_Implementation/src/object.c
@@ -0,0 +1,27 @@
+#include<object.h>
+void send_a_command(unsigned char command)
+{
+    PORTB = command;
+    PORTD &= ~ (1<<registerselection);
+    PORTD |= 1<<enable;
+    _delay_ms(8);
+    PORTD &= ~1<<enable;
+    PORTB = 0;
+}
+
+void send_a_character(unsigned char character)
+{
+    PORTB = character;
+    PORTD |= 1<<registerselection;
+    PORTD |= 1<<enable;
+    _delay_ms(8);
+    PORTD &= ~1<<enable;
+    PORTB = 0;
+}
+void send_a_string(char *string_of_characters)
+{
+    while(*string_of_characters > 0)
+    {
+        send_a_character(*string_of_characters++);
+    }
+}
