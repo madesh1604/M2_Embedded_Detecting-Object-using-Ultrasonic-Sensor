ffb4add85321fdc51e1bd119aea653ebb0f614dc
Sankar Narayanan <91029826+madesh1604@users.noreply.github.com>
2021-12-01 22:40:47 +0530

    Update inc.h

--- 3_Implementation/inc.h
+++ 3_Implementation/inc.h
@@ -1,3 +1,14 @@
+/**
+ * @file object.h
+ * @author madesh1604
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-1
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
 #ifndef __OBJECT_H_
 #define __OBJECT_H_
 #define F_CPU 1000000
@@ -7,6 +18,7 @@
 #define enable            5
 #define registerselection 6
 
+//Function Prototype
 void send_a_command(unsigned char command);
 void send_a_character(unsigned char character);
 void send_a_string(char *string_of_characters);
